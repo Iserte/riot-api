feb24a6e2650369180413f66a7fadc7d4bf4bbe8
Gustavo Iserte Bonfim <gustavo@dvdsp.com.br>
2019-11-29 11:18:03 -0300

    Updates

---
+++ precache-manifest.71d1a05e9a1f369fb86f9248c4415809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7642d69b84cf62c62748f31e02324d",
+    "url": "/riot-api/index.html"
+  },
+  {
+    "revision": "9078d17891601ddd89f7",
+    "url": "/riot-api/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e242bd80d38db081faa8",
+    "url": "/riot-api/static/js/2.b50da3dd.chunk.js"
+  },
+  {
+    "revision": "9078d17891601ddd89f7",
+    "url": "/riot-api/static/js/main.7e4f3a10.chunk.js"
+  },
+  {
+    "revision": "0d8b74efee442c5622c8",
+    "url": "/riot-api/static/js/runtime-main.4acb548b.js"
+  }
+]);
